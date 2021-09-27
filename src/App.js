10cf91d404fe55c5237007aee95164247fe28e4d
robbie jr <malungarobbie@gmail.com>
2021-09-27 13:04:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// import pages
+import Home from './Home';
+import Login from "./auth/Login";
+import Dashboard from "./admin/Dashboard";
+import ProductReview from "./ProductReview";
+import Analysis from "./admin/Analysis";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path = {['/'] } component = { Home }/>
+            <Route exact path = {['/admin'] } component = { Login }/>
+              <Route exact path = {['/logout'] } component = { Login }/>
+            <Route path = { '/dashboard' } component = { Dashboard }/>
+            <Route path = { '/analysis' } component = { Analysis }/>
+              <Route path = { '/products' } component = { Analysis }/>
+            <Route path = { '/product-review' } component = { ProductReview }/>
+          </Switch>
+        </BrowserRouter>
+      </div>
   );
 }
+
 
 export default App;
